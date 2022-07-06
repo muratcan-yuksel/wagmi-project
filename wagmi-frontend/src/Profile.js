6c89bca0e53caf6509173a6347d7658f5abd3640
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-07-06 03:35:22 +0300

    wtf is buffer is not defined error?

--- wagmi-frontend/src/Profile.js
+++ wagmi-frontend/src/Profile.js
@@ -1,7 +1,50 @@
 import React from "react";
+import {
+  useAccount,
+  useConnect,
+  useDisconnect,
+  useEnsAvatar,
+  useEnsName,
+} from "wagmi";
 
 const Profile = () => {
-  return <div>Profile profile component</div>;
+  const { address, connector, isConnected } = useAccount();
+  const { data: ensAvatar } = useEnsAvatar({ addressOrName: address });
+  const { data: ensName } = useEnsName({ address });
+  const { connect, connectors, error, isLoading, pendingConnector } =
+    useConnect();
+  const { disconnect } = useDisconnect();
+
+  if (isConnected) {
+    return (
+      <div>
+        <img src={ensAvatar} alt="ENS Avatar" />
+        <div>{ensName ? `${ensName} (${address})` : address}</div>
+        <div>Connected to {connector.name}</div>
+        <button onClick={disconnect}>Disconnect</button>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      {connectors.map((connector) => (
+        <button
+          disabled={!connector.ready}
+          key={connector.id}
+          onClick={() => connect({ connector })}
+        >
+          {connector.name}
+          {!connector.ready && " (unsupported)"}
+          {isLoading &&
+            connector.id === pendingConnector?.id &&
+            " (connecting)"}
+        </button>
+      ))}
+
+      {error && <div>{error.message}</div>}
+    </div>
+  );
 };
 
 export default Profile;
